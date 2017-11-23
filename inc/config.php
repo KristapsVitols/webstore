4554e95ba6b62181fe2141e678532c79e962e845
KristapsVitols <vitolskristaps@inbox.lv>
2017-11-23 14:01:45 +0200

    hello

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,6 @@
 <?php
 //Connect to DB PDO
-define("DB_HOST", 'localhost');
+define("DB_HOST", 'us-cdbr-iron-east-05.cleardb.net');
 define("DB_USER", 'b9502f77308eab');
 define("DB_PASS", '83ec21e9');
 define("DB_NAME", 'heroku_b53c6be955ed9de');
